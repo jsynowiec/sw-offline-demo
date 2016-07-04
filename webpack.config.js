a0f870c1163ce7f813faa06f42913a5ee29a1cd4
Jakub Synowiec <jakub.synowiec@gmail.com>
2016-07-04 14:13:22 +0200

    Add Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/app.js',
+    sw: './src/sw.js',
+  },
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: '[name].js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract('css!less'),
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css', {
+      allChunks: true,
+    }),
+  ],
+};
